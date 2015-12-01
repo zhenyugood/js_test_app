038173b5d45acde19dd7420e6a68e16e517d142b
zhenyugood <315741718@qq.com>
2015-12-01 16:29:47 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,15 +1,43 @@
-var myId=new Array();
+
 var i=0;
-var myDelete=new Array();
-function search(){
- console.log("hello search");
+
+    function search(){
+ //document.getElementById("div0").style.display="none"
+        
+    var getdiv = document.getElementById("myDiv");  
+    var outdiv = getdiv.getElementsByTagName("div");  
+    for(var i=0;i<outdiv.length;i++){
+       var inText=outdiv[i].getElementsByTagName("input"); 
+       var reg=new RegExp(document.getElementById("textSearch").value);
+       for(var j=0;j<inText.length;j++){
+       if(inText[j].type=="text"&&reg.test(inText[j].value)){
+          outdiv[i].style.display="";
+    }
+       else if(inText[j].type=="text"&&document.getElementById("textSearch").value==" ") outdiv[i].style.display="";
+    else outdiv[i].style.display="none";
+  }
+    }
+
+   // for (i = 0; i < outdiv.length; i++) {  
+ //       console.log(outdiv[i].getElementsByTagName("text").value); 
+        
+ //   }  
+         
+}
+
+function setVisible(){
+   var getdiv = document.getElementById("myDiv");  
+    var outdiv = getdiv.getElementsByTagName("div");  
+   for(var i=0;i<outdiv.length;i++){
+      outdiv[i].style.display="";
+    }
 }
 
 
-
 function addNewButton(){
-    myId.push(i);
+  //  myId.push(i);
     var myText = text.value;
+    clearText();
     var inputButton =document.createElement("input");
     var inputText =document.createElement("input");
     inputButton.type="button";
@@ -22,38 +50,52 @@ function addNewButton(){
     div.innerHTML="<br/>";  
     div.id="div"+i;           
     myDiv.appendChild(div);
-    var mydiv=document.getElementById("div"+i);
-    mydiv.appendChild(inputButton);
-    mydiv.appendChild(inputText);
+  //  var mydiv=document.getElementById("div"+i);
+    div.appendChild(inputButton);
+    div.appendChild(inputText);
+    inputButton.onclick=function(){
+    div.parentNode.removeChild(div);
+    };
 }
 
 
-function removeButton(i){
- var elem = document.getElementById("div"+i);
-          elem.parentNode.removeChild(elem);
-          return false;
+function clearText(){
+  var text= document.getElementById("text");
+  text.value="";
 }
 
+
+function keyPress(){
+  var e = window.event || arguments.callee.caller.arguments[0];
+         if (e && e.keyCode == 13 ) {
+            addNewButton();
+         }
+}
+
+//function removeButton(i){
+// var elem = document.getElementById("div"+i);
+//          elem.parentNode.removeChild(elem);
+ //         return false;
+//}
 
 
 function prepareEventHandlers() {
     var add = document.getElementById("add");
     var text= document.getElementById("text");
-
-    
-    text.addEventListener("keyup",search);
-
+    var textSearch= document.getElementById("textSearch");
+    textSearch.addEventListener("click",clearText);
+    textSearch.addEventListener("keyup",search);
+    text.addEventListener("keyup",keyPress);
 
     add.onclick =  function() {
-    addNewButton();      
-    myDelete[i] = document.getElementById("button"+i);
+    addNewButton();  
+/*    myDelete[i] = document.getElementById("button"+i);
 (function(i){
 myDelete[i].onclick=function(){
          removeButton(i);
-        };})(i)
-     i++;
-    }
-
+        };})(i) */
+  i++;
+    };
 } 
 
 
