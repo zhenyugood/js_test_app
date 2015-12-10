223b6d4f43d1ab1b7897ebc432dfdd895b3d4db8
zhenyugood <315741718@qq.com>
2015-12-10 13:09:58 +0800

    Create js3.js

---
+++ js3.js
@@ -0,0 +1,85 @@
+
+var HTMLEditer = function(){
+  this.INDEX = 0;
+  this.textSearch = document.getElementById("textSearch");
+  this.myDiv = document.getElementById("myDiv");
+  this.addButton = document.getElementById("addButton");
+  this.textInput = document.getElementById("textInput");
+  
+  this.clearMyInputText = function(){
+    this.textInput.value = "";
+  };
+  
+  this.addNewButton = function(){
+    var myInputText = this.textInput.value;
+//    this.clearMyInputTextCallBack(this.clearMyInputText());
+    var newButton = document.createElement("input");
+    var newText = document.createElement("input");
+    newButton.type = "button";
+    newButton.id = "button"+this.INDEX;
+    newButton.value ="delete";
+    newText.type = "text";
+    newText.id = "text"+this.INDEX;
+    newText.value = myInputText;
+    newText.style.fontSize = "2em";
+    var li = document.createElement("li");
+    li.id = "li"+this.INDEX;           
+    this.myDiv.appendChild(li);
+    li.appendChild(newButton);
+    li.appendChild(newText);
+    this.INDEX++;
+    newButton.onclick=function(){
+    li.parentNode.removeChild(li);
+    };
+  };
+  
+  this.search = function(){
+    var listOfButtonAndText =  myDiv.querySelectorAll("li>input");
+    var reg=new RegExp(textSearch.value);
+    for(var numInList=0;numInList<listOfButtonAndText.length;numInList++){
+      if(listOfButtonAndText[numInList].type==="text"&&reg.test(listOfButtonAndText[numInList].value)){
+        listOfButtonAndText[numInList].parentElement.style.display="";
+      }
+      else listOfButtonAndText[numInList].parentElement.style.display="none";
+    }
+  };
+  
+  //call back functions are as fallowed
+  this.clearMyInputTextCallBack = function(cb){
+    setTimeout(function(){
+      cb();
+    },2000);
+  };
+  this.addingNewButton = function(cb){
+    setTimeout(function(){
+      cb();
+    },2000);
+  };
+  
+};
+
+
+
+function prepareEventHandlers() {
+  var myEditer = new  HTMLEditer();
+  myEditer.addButton.onclick = function(){
+  myEditer.addingNewButton(myEditer.addNewButton());
+  };
+  myEditer.textSearch.addEventListener("keyup",myEditer.search);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.onload = function() {
+    prepareEventHandlers();
+};  
